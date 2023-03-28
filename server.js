84c6ae878911a92f0a0b40b2cfe2b7b0643e12fa
saipepu <saipepu.mdy257@gmail.com>
2023-03-28 17:43:50 +0700

    changes

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use('/api/judges', JudgeRoutes)
 // const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "http://localhost:3001",
     method: ["GET","POST"],
     credentials: true
   }
@@ -50,6 +50,7 @@ const start = async () => {
   // const collection = db.collection("Judge");
   const db = client.db("JudgeHub");
   const collection = db.collection("allteams");
+  console.log(collection);
   io.on('connection', (socket) => {
     console.log('one client connected')
     
